8e49295e34ff4bffc40bd8313c390f2eb4e667df
Fernely nunez sosa <151415687+Ferny121991@users.noreply.github.com>
2025-07-17 21:28:37 -0400

    Agregando base para producción en Hostinger

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './', // 👈 IMPORTANTE para producción en Hostinger
+
   define: {
     'import.meta.env.VITE_APP_BUILD_TIME': JSON.stringify(new Date().getTime())
   },
@@ -12,7 +14,6 @@ export default defineConfig(({ mode }) => ({
   },
   server: {
     proxy: {
-      // Proxy API requests to PHP server
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
@@ -20,4 +21,4 @@ export default defineConfig(({ mode }) => ({
       }
     }
   },
-}));
+}))
